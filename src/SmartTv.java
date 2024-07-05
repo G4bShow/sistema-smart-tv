b6c6cdbf0b98f144c74e15c7e161739d45d486cf
Gabshow <gs4876330@gmail.com>
2024-07-04 23:02:20 -0300

    add-canal-atual

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -3,6 +3,27 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+     
+    public void mudarCanal( int novocanal){
+        canal = novocanal;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminuirCanal(){
+        canal--;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+    }
+
+    public void diminuiVolume(){
+        volume--;
+    }
+
     public void ligar(){
         ligada = true;
     }
--- src/Usuario.java
+++ src/Usuario.java
@@ -3,6 +3,17 @@ public class Usuario {
         
         SmartTv smartTv = new SmartTv();
 
+        smartTv.diminuiVolume();
+        smartTv.diminuiVolume();
+        smartTv.diminuiVolume();
+        smartTv.aumentarVolume();
+
+        System.out.println("Canal Atual" + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+        
+        System.out.println("Canal Atual" + smartTv.canal);
+
         System.out.println("TV Ligada" + smartTv.ligada);
         System.out.println("Canal Atual" + smartTv.canal);
         System.out.println("Volume Atual" + smartTv.volume);
